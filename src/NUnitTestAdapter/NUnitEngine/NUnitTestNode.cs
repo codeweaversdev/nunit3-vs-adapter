12b058c2535048dc8024f82b7fd202ae6a0062e0
Developer <Developer@HOBGOBLIN>
2022-01-17 22:21:07 +0000

    id hack

--- src/NUnitTestAdapter/NUnitEngine/NUnitTestNode.cs
+++ src/NUnitTestAdapter/NUnitEngine/NUnitTestNode.cs
@@ -14,7 +14,21 @@ namespace NUnit.VisualStudio.TestAdapter.NUnitEngine
     public abstract class NUnitTestNode : INUnitTestNode
     {
         protected XmlNode Node { get; set; }  // Need to be protected, but still the outputnodes are XmlNode
-        public virtual string Id => Node.GetAttribute("id");
+        public virtual string Id
+        {
+            get
+            {
+                var rawId = Node.GetAttribute("id");
+                if (rawId == null)
+                    return null;
+
+                if (!rawId.StartsWith("0-"))
+                    return "0" + rawId.Substring(1);
+
+                return rawId;
+            }
+        }
+
         public string FullName => Node.GetAttribute("fullname");
         public string Name => Node.GetAttribute("name");
         public bool IsNull => Node == null;
